d1a5618de4fe89bbaca581480fa2a1ecf9d119b4
Brent Anderson <me@brentjanderson.com>
2014-08-07 01:43:57 -0600

    Update to 1.1.4

--- package.js
+++ package.js
@@ -1,8 +1,11 @@
 Package.describe({
-	summary: "The fantastic Buzz javascript sound library repackaged for Meteor"
+	summary: "The fantastic Buzz javascript sound library repackaged for Meteor",
+  version: "1.1.4",
+  git: "https://github.com/brentjanderson/meteor-buzz.git"
 });
 
 Package.on_use(function (api) {
+  api.versionsFrom("METEOR-CORE@0.9.0-atm");
 	
 	if (api.export) // ensure backwards compatibility with Meteor pre-0.6.5
     		api.export('BUZZ');
